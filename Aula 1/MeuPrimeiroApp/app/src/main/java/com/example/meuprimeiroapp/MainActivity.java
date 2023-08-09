f36187e4e751c57e0f6157a3f32deb2bf66faf25
Vitor Hugo Schiphorst <vitor.s13@aluno.ifsc.edu.br>
2023-08-09 11:32:05 -0300

    Aula 3

--- Aula 1/MeuPrimeiroApp/app/src/main/java/com/example/meuprimeiroapp/MainActivity.java
+++ Aula 1/MeuPrimeiroApp/app/src/main/java/com/example/meuprimeiroapp/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.meuprimeiroapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -11,15 +17,40 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v("Ciclo de Vida Activity: ", "onCreate");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStart() {
+
         super.onStart();
+        Log.v("Ciclo de Vida Activity: ", "onStart");
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        Log.v("Ciclo de Vida Activity: ", "onResume");
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        Log.v("Ciclo de Vida Activity: ", "onPause");
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
+
         super.onStop();
+        Log.v("Ciclo de Vida Activity: ", "onStop");
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
     }
+
 }
